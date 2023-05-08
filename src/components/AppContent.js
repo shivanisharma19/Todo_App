760140c3b48bcb3f30a2f980ced2910587dd77b0
Fujitsu <shivanisharma1941995@gmail.com>
2023-05-08 23:40:14 +0530

    corrected the css to read item added time formatter

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import '../style/appContent.css'
-
+import React from "react";
+import { useSelector } from "react-redux";
+import "../style/appContent.css";
+import { format } from "date-fns";
 
 const AppContent = () => {
-    const todoList = useSelector((state) => state.todo.todoList)
-    const sortedTodoList =[...todoList]
-    sortedTodoList.sort((a, b) => new Date(b.time) - new Date(a.time))
-    return <div className='todo_wrap'>
-        {sortedTodoList && sortedTodoList.length>0
-           ? sortedTodoList.map((todo)=>  
-           <div key={todo.id} className='todo_item'>
-            <div className = 'todo_details'>
-               <p className='todo_text'>
-                   {todo.title}
-               </p>
-               <p className='todo_time'>
-                   {todo.time}
-               </p>
-           </div>
-          </div>)
-           : <p className='emptyText'>No Todos</p>
-        }
+  const todoList = useSelector((state) => state.todo.todoList); //how you get value from store
+  const sortedTodoList = [...todoList]; //... means all the vales will be used
+  sortedTodoList.sort((a, b) => new Date(b.time) - new Date(a.time)); // compare the time
+  return (
+    <div className="todo_wrap">
+      {sortedTodoList && sortedTodoList.length > 0 ? (
+        sortedTodoList.map((todo) => (
+          <div key={todo.id} className="todo_item">
+            <div className="todo_details">
+              <p className="todo_text">{todo.title}</p>
+              <p className="todo_time">
+                {format(new Date(todo.time), "p, dd/MM/yyy")}
+              </p>
+            </div>
+          </div>
+        ))
+      ) : (
+        <p className="emptyText">No Todos</p>
+      )}
     </div>
-}
+  );
+};
 
-export default AppContent
+export default AppContent;
